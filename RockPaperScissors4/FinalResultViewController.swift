993c7c17ae12bb3f4b8927baf746bfc2d8a16ed2
Yuting Gao <0711candy@gmail.com>
2022-05-11 17:39:57 -0700

    added user name input

--- RockPaperScissors4/FinalResultViewController.swift
+++ RockPaperScissors4/FinalResultViewController.swift
@@ -24,46 +24,24 @@ class FinalResultViewController: UIViewController{
     var greetingText: String = ""
     
     
+    
+    
     @IBAction func againPressed(_ sender: Any) {
-        //poproot instntialize a new game
-        //dismiss self
-        //print("dismissing finalResult")
-        //dismiss(animated: true, completion: nil)
-        //self.navigationController?.popToRootViewController(animated: true)
-        //delegate?.modalVCDidCancel()
         navigationController?.popToRootViewController(animated: true)
-        //navigationController?.popViewController(animated: true)
-//        let homePageVC = storyboard?.instantiateViewController(withIdentifier: "homePage") as! HomeViewController
-//        homePageVC.modalPresentationStyle = .fullScreen
-//        homePageVC.modalTransitionStyle = .crossDissolve
-//        present(homePageVC, animated: true)
     }
     
     
     @IBAction func quitPressed(_ sender: UIButton) {
         print("quit pressed")
-        //delegate?.storeScore()
-//        let newScore = UserScore(context: context)
-//        newScore.wins = totalWins
-//        newScore.ties = totalTies
-//        newScore.losses = totalLosses
-//        
-//        do {
-//            try context.save()
-//        } catch {
-//        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("notifying delegate")
         delegate?.storeScore()
-        //print("notifying delegate")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         greeting.text = greetingText
-        //self.navigationItem.setHidesBackButton(true, animated: true)
     }
     
 }
--- RockPaperScissors4/RecordViewController.swift
+++ RockPaperScissors4/RecordViewController.swift
@@ -11,7 +11,26 @@ import UIKit
 
 class RecordViewController: UIViewController{
     
+    @IBOutlet weak var userNameInput: UITextField!
     
+    @IBOutlet weak var prompt: UILabel!
+    
+    
+    
+    
+    @IBAction func onConfirmName(_ sender: Any) {
+        guard let userName: String = userNameInput.text else {
+            prompt.text = "Error! Please enter a valid name."
+            return
+        }
+        
+        guard !userName.isEmpty else {
+            prompt.text = "Name cannot be empty!"
+            return
+        }
+        
+        prompt.text = userName
+    }
     @IBAction func onViewScorePressed(_ sender: Any) {
         performSegue(withIdentifier: "toScorefromUserInputSegue", sender: self)
     }
